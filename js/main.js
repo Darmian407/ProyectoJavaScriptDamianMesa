7a49991fdbb7783ed94c5efc0eea6f1213b326f2
Darmian407 <damianmesa407@gmail.com>
2022-06-21 10:08:12 -0300

    Clase 1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* console.log('sacar un mensaje por consola 4');  */
+console.log("Se inicio el Proyecto");
+
+//Declaration
+
+/* const nombre = "Damian"; const alumno1 = "Juan Carlos";
+
+let saludo;
+saludo = "Hola Mundo"; 
+
+let saludo1 = "Hola Mundo"; */
+
+let nombre = prompt("Ingrese su Nombre");
+let edad = prompt("Ingrese su Edad");
+console.log("Nombre: " + nombre + " de " + edad + " Años");
+alert("Bienvendi@ " + nombre);
